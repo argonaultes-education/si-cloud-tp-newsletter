de140e6df00f4e5ec0aa9c61ddf3a6ae941d7595
argonaultes <gael@argonaultes.fr>
2025-04-24 09:53:54 +0200

    test email

--- newsletter_test.py
+++ newsletter_test.py
@@ -1,7 +1,7 @@
 import flask_unittest
 import unittest
 from newsletter import app as my_app
-
+import re
 
 class TestWelcome(flask_unittest.ClientTestCase):
 
@@ -19,9 +19,26 @@ class TestWelcome(flask_unittest.ClientTestCase):
             rv = client.get(f'/welcome?identifier={identifier}')
             self.assertInResponse(b'Give your identifier', rv)
 
-    def test_welcome(self, client):
+    def test_welcome_without_identifier(self, client):
         rv = client.get('/welcome')
         self.assertInResponse(b'Give your identifier', rv)
+
+class TestSubscribe(flask_unittest.ClientTestCase):
+    
+    app = my_app
+
+    def test_valid_email(self, client):
+        emails = ['gael@hotmail.com', 'super@caramail.com', 'gael@argonaultes.fr']
+        for email in emails:
+            rv = client.get(f'/subscribe?email={email}')
+            self.assertTrue(re.search(email, rv.data.decode('UTF-8')), email)
+
+    def test_wrong_email(self, client):
+        emails = ['gael@gael.com', 'hello@news.fr', 'thisisnotanemail']
+        for email in emails:
+            rv = client.get(f'/subscribe?email={email}')
+            self.assertFalse(re.search(email, rv.data.decode('UTF-8')), email)
+
 
 if __name__ == '__main__':
     unittest.main()
